35b6248800fb483bb4453c00958d6124c6cf2e5e
VictorXiao <843731025@qq.com>
2016-09-06 21:58:15 +0800

    Initial commit

---
+++ app/src/main/java/com/victor_xiao/activitytransfer/SecondActivity.java
@@ -0,0 +1,28 @@
+package com.victor_xiao.activitytransfer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Victor_Xiao on 16/08/31.
+ */
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.secondactivity);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
